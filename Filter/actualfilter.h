137b4e557179a4a93cfc0e0d25b31e5a8059c84b
Adam.Rackis <adam.rackis@riotgames.com>
2019-08-01 10:08:18 -0500

    Simplify numeric fields

--- Filter/actualfilter.h
+++ Filter/actualfilter.h
@@ -77,14 +77,14 @@ std::shared_ptr<OrFilter<Of>> operator ||(const std::shared_ptr<ActualFilter<Of,
 
 //            OPERATOR <
 
-template <typename Of, typename T, typename U>
-decltype (auto) operator<(const NumericField<Of, T> &f, const U &val)
+template <typename Of, typename T, typename U, typename = std::enable_if_t<std::is_arithmetic_v<U>>>
+decltype (auto) operator<(const Field<Of, T> &f, const U &val)
 {
     return val < f;
 }
 
-template <typename Of, typename T, typename U>
-decltype (auto) operator<(const U &val, const NumericField<Of, T> &f)
+template <typename Of, typename T, typename U, typename = std::enable_if_t<std::is_arithmetic_v<U>>>
+decltype (auto) operator<(const U &val, const Field<Of, T> &f)
 {
     using AdjustedType = std::common_type_t<T, U>;
     using FilterType = ActualFilter<Of, AdjustedType>;
