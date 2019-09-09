a5c1b48e727080d2d52f645754eefa963b3add9a
Adam.Rackis <adam.rackis@riotgames.com>
2019-09-09 18:56:00 -0500

    tweaks

--- DataDisplay/listViewManager.h
+++ DataDisplay/listViewManager.h
@@ -22,14 +22,14 @@ QSize ViewDelegate<T>::sizeHint(const QStyleOptionViewItem &, const QModelIndex 
 {
     auto height = this->widgets[index.row()]->getWidget()->sizeHint().height();
     qDebug() << "New Height" << height;
-    return QSize(0, height); //enter your values here
+    return QSize(0, height);
 }
 
 template <typename T, typename U, typename = std::enable_if_t<std::is_base_of_v<ListWidgetItem<T>, U>>>
 class ListViewManager {
-
 public:
-    ListViewManager(QListView *listView, size_t size) : maxSize(size), listModel(new ListModel<T>{}) {
+    ListViewManager(QListView *listView, size_t size) : maxSize(size), listModel(new ListModel<T>{})
+    {
         this->listView = listView;
         this->listWidgetPool = std::vector<std::shared_ptr<U>>(size);
 
@@ -49,28 +49,11 @@ public:
 
     virtual ~ListViewManager() {}
 
-    void setData(const std::vector<T> &results)
-    {
-        this->listModel->newData(results);
-        for (size_t i = 0; i < results.size(); i++)
-        {
-            auto w = this->listWidgetPool[i];
-            w->bind(results[i]);
-            this->listView->setIndexWidget(this->listModel->index(i), w->getWidget());
-        }
-    }
+    void setData(const std::vector<T> &results);
 
 private:
 
-    void _updateListViewSizing()
-    {
-        QSize size = this->listView->viewport()->size();
-        size.setHeight(size.height() + 1);
-        this->listView->viewport()->resize(size);
-
-        size.setHeight(size.height()-1);
-        this->listView->viewport()->resize(size);
-    }
+    void _updateListViewSizing();
 
     QListView *listView;
     ListModel<T> *listModel;
@@ -78,3 +61,26 @@ private:
     size_t maxSize;
     std::function<void()> updateListViewSizing;
 };
+
+template <typename T, typename U, typename V>
+void ListViewManager<T, U, V>::setData(const std::vector<T> &results)
+{
+    this->listModel->newData(results);
+    for (size_t i = 0; i < results.size(); i++)
+    {
+        auto w = this->listWidgetPool[i];
+        w->bind(results[i]);
+        this->listView->setIndexWidget(this->listModel->index(i), w->getWidget());
+    }
+}
+
+template <typename T, typename U, typename V>
+void ListViewManager<T, U, V>::_updateListViewSizing()
+{
+    QSize size = this->listView->viewport()->size();
+    size.setHeight(size.height() + 1);
+    this->listView->viewport()->resize(size);
+
+    size.setHeight(size.height()-1);
+    this->listView->viewport()->resize(size);
+}
